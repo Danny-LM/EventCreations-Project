69e00fcfa47c26dd15dca08338e62c6d3d53e9e8
Tu Nombre <tuemail@example.com>
2025-02-07 10:25:01 -0600

    07/02 - Adición de secciones

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -22,6 +22,18 @@
                     <li class="nav-item">
                         <a class="nav-link active" aria-current="page" href="#">Home</a>
                     </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Objective</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Services</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Login</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Create Account</a>
+                    </li>
                 </ul>
             </div>
         </div>

98d033446802d74f75036caf83cbd354788f2724
{Alex-rivs} <{f.rivera.isw@unipolidgo.edu.mx}>
2025-02-12 11:22:40 -0600

    {Nombre nomas}

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -7,12 +7,13 @@
     <link rel="stylesheet" href="https://necolas.github.io/normalize.css/latest/normalize.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="{{asset('css/custom.css')}}">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
 </head>
 <body>
     <nav class="navbar navbar-expand-lg navbar-light bg-light sticky-top">
         <div class="container">
             <a class="navbar-brand" href="#">
-                EventCreations
+                LOGO
             </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>

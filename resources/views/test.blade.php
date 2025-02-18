46801b1c444988daf7bdd2176745a643187a4a4f
Tu Nombre <tuemail@example.com>
2025-02-18 09:54:42 -0600

    solo para subir avances

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -3,43 +3,18 @@
 
 <link rel="stylesheet" href="{{ asset('css/custom.css') }}">
 
-<h1 style="display: flex; justify-content: center; align-items: center; height: 20vh; text-align: center;">TEST PAGE</h1>
-
-<section class="section" id="Test-Texts">
+<section class="section" id="Test-Section">
   <div class="container">
-    <h2>Test Texts</h2>
-
-    <div>
-      <h2>Heading 2</h2>
-    </div>
-    <div>
-      <h2>Heading 2</h2>
-    </div>
-    <div>
-      <h2>Heading 2</h2>
-    </div>
-    <div>
-      <h2>Heading 2</h2>
-    </div>
-  </div>  
-</section>
-
-<section class="section" id="Test-Buttons">
-  <div class="container">
-    <h2>Test Buttons</h2>
-
+    <h1>Test Buttons</h1>
     <div>
         <a href="#" class="btn-primary">Primary Button</a>
     </div>
-    <br>
     <div>
         <a href="#" class="btn-secondary">Secondary Button</a>
     </div>
-    <br>
     <div>
         <a href="#" class="btn-tertiary">Tertiary Button</a>
     </div>
-    <br>
     <div>
         <a href="#" class="btn-quaternary">Quaternary Button</a>
     </div>

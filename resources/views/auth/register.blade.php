f5d97801f8b88e495a1811454b565dd4fd62093a
{Alex-rivs} <{f.rivera.isw@unipolidgo.edu.mx}>
2025-02-28 13:39:59 -0600

    coemntado seccion merge

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -85,3 +85,38 @@
 </div>
 @endsection
 
+
+
+<!-- David version -->
+
+<!--
+<div class= 'container'>
+<h1> Sing Up</h1>
+ <div class="row g-2">
+  <div class="col-md">
+    
+      <div class="form-floating">   
+      <input type="text" class="form-control" id="username" placeholder=" " value="">
+      <label for="username">Your username</label></div>
+      
+      <div class="form-floating">
+        
+      <input type="text" class="form-control" id="email" placeholder="" value="">
+      <label for="email">Your email</label></div>
+      
+      <div class="form-floating">
+      <input type="password" class="form-control" id="pass" placeholder="" value="">
+      <label for="pass">Your Password</label></div>
+      
+      <div class="form-floating">
+      <input type="password" class="form-control" id="confirm_pass" placeholder="" value="">
+      <label for="confirm_pass">Confirm Password</label></div>
+
+      <div class="form-floating">
+      <button  class="form-control">Log in</button></div>
+      <center>
+      <div class='line'>
+        <span class='text'>OR</span>
+    </div></center>*/
+>>>>>>> 46935727354d1fa68175abbbd3f2d672e3d3e5bb
+/*

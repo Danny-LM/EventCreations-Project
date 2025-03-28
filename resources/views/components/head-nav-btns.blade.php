934fdd38fc75bdeb4080a3e4b863c5c59fc16b4f
Tu Nombre <tuemail@example.com>
2025-03-28 16:24:24 -0600

    Refactor CSS variables and styles; update background colors, improve text styles, and add new text utility classes

--- resources/views/components/head-nav-btns.blade.php
+++ resources/views/components/head-nav-btns.blade.php
@@ -2,24 +2,25 @@
 <!-- Let all your things have their places; let each part of your business have its time. - Benjamin Franklin -->
 
 <!-- 1: Header Nav Buttons -->
-<div class="header-nav d-flex justify-content-end align-items-center p-2">
+<div class="header-nav d-flex justify-content-end align-items-center p-3">
     <ul class="navbar-nav d-flex flex-row">
-        <!-- Settings -->
-        <li class="nav-item mx-3">
+        <!-- User Profile -->
+        <li class="nav-item mx-3 d-flex align-items-center">
             <a class="nav-link" href="#">
-                <i class="lni lni-gear-1"></i>
+                <i class="lni lni-user-4 align-middle"></i>
+            </a>
+        </li>
+        
+        <!-- Settings -->
+        <li class="nav-item mx-3 d-flex align-items-center">
+            <a class="nav-link" href="#">
+                <i class="lni lni-gear-1 align-middle"></i>
             </a>
         </li>
         <!-- Notifications -->
-        <li class="nav-item mx-3">
+        <li class="nav-item mx-3 d-flex align-items-center">
             <a class="nav-link" href="#">
-                <i class="lni lni-bell-1"></i>
-            </a>
-        </li>
-        <!-- User Profile -->
-        <li class="nav-item mx-3">
-            <a class="nav-link" href="#">
-                <i class="lni lni-user-4"></i>
+                <i class="lni lni-bell-1 align-middle"></i>
             </a>
         </li>
     </ul>

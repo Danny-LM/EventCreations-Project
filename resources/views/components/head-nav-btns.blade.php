3bfec6c5ea3a672daf33bfc50033fe54fa44c9bc
Tu Nombre <tuemail@example.com>
2025-03-28 22:06:11 -0600

    Add search filter bar component and styles; enhance user interface with search and filter functionality

--- resources/views/components/head-nav-btns.blade.php
+++ resources/views/components/head-nav-btns.blade.php
@@ -2,23 +2,28 @@
 <!-- Let all your things have their places; let each part of your business have its time. - Benjamin Franklin -->
 
 <!-- 1: Header Nav Buttons -->
-<div class="header-nav d-flex justify-content-end align-items-center p-3">
-    <ul class="navbar-nav d-flex flex-row">
+<div class="header-nav d-flex justify-content-between align-items-center p-3">
+    <!-- Back btn -->
+    <a class="nav-link d-flex align-items-center me-3" href="#">
+        <i class="lni lni-arrow-left align-middle"></i>
+    </a>
+
+    <ul class="navbar-nav d-flex flex-row mb-0 align-items-center">
         <!-- User Profile -->
-        <li class="nav-item mx-3 d-flex align-items-center">
+        <li class="nav-item mx-3">
             <a class="nav-link" href="#">
                 <i class="lni lni-user-4 align-middle"></i>
             </a>
         </li>
         
         <!-- Settings -->
-        <li class="nav-item mx-3 d-flex align-items-center">
+        <li class="nav-item mx-3">
             <a class="nav-link" href="#">
                 <i class="lni lni-gear-1 align-middle"></i>
             </a>
         </li>
         <!-- Notifications -->
-        <li class="nav-item mx-3 d-flex align-items-center">
+        <li class="nav-item mx-3">
             <a class="nav-link" href="#">
                 <i class="lni lni-bell-1 align-middle"></i>
             </a>
---
+++ resources/views/components/search-filter-bar.blade.php
@@ -0,0 +1,24 @@
+
+<!-- The whole future lies in uncertainty: live immediately. - Seneca -->
+
+<div class="d-flex align-items-center gap-2 mx-1 p-4">
+    <!-- Search Bar -->
+    <div class="input-group shadow search-bar mx-2">
+        <span class="input-group-text cst-border-1">
+            <i class="lni lni-search-1"></i>
+        </span>
+        <input type="text" class="form-control cst-border-2" placeholder="Search Here...">
+    </div>
+
+    <!-- Filter Button -->
+    <button class="btn-search-action btn shadow mx-2">
+        Filter
+        <i class="lni lni-sliders-horizontal-square-2"></i>
+    </button>
+
+    <!-- Order By Button -->
+    <button class="btn-search-action btn shadow mx-2">
+        Order by
+        <i class="lni lni-align-text-center"></i>
+    </button>
+</div>
